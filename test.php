a887c6d6a48c1ced54f1427e86eb8bd9e5c0bef2
Adam Macdonald <amac03123@gmail.com>
2017-04-19 12:58:34 -0400

    test

--- test.php
+++ test.php
@@ -1,6 +1,8 @@
 <?php
-$str ='<p><a href="http://localhost/sandbox/pearls/users/1002/" class="mentionned" target="_blank">@test</a>&nbsp;<a href="http://localhost/sandbox/pearls/users/1001/" class="mentionned" target="_blank">@michael2</a>&nbsp;@raalspd</p>';
+//$str ='<p><a href="http://localhost/sandbox/pearls/users/1002/" class="mentionned" target="_blank">@test</a>&nbsp;<a href="http://localhost/sandbox/pearls/users/1001/" class="mentionned" target="_blank">@michael2</a>&nbsp;@raalspd</p>';
+//
+//preg_match_all('/(^|\s|&nbsp;)(@\w+)/', strip_tags($str), $mentions);
+//$results = array_unique($mentions[0]);
+//print_r($results);
 
-preg_match_all('/(^|\s|&nbsp;)(@\w+)/', strip_tags($str), $mentions);
-$results = array_unique($mentions[0]);
-print_r($results);
+echo phpinfo();
